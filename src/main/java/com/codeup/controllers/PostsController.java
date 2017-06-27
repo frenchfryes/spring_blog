93d1a0824e1125144404758199d095a823208118
frenchfryes <derekwfrye@gmail.com>
2017-06-27 15:39:42 -0500

    Added error pages

--- src/main/java/com/codeup/controllers/PostsController.java
+++ src/main/java/com/codeup/controllers/PostsController.java
@@ -5,10 +5,19 @@ import com.codeup.models.User;
 import com.codeup.repositories.UsersRepository;
 import com.codeup.svcs.PostSvc;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.domain.Page;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.awt.print.Pageable;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Paths;
 import java.util.List;
 
 import static org.springframework.data.jpa.domain.AbstractPersistable_.id;
@@ -40,18 +49,56 @@ public class PostsController {
 
     @GetMapping("/posts/{id}")
     public String viewIndividualPost(@PathVariable long id, Model model) {
-        // Inside the method that shows an individual post, create a new post object and pass it to the view.
+
         Post post = postSvc.findOne(id);
         model.addAttribute("post", post);
         return "posts/show";
     }
 
+    @GetMapping("/posts/create")
+    public String showPostForm(Model model) {
+        model.addAttribute("post", new Post());
+        return "posts/create";
+    }
+
     @PostMapping("/posts/create")
-    public String create(@ModelAttribute Post post, Model model) {
-        model.addAttribute("post", post);
-        post.setOwner(userRepository.findOne(1L));
+    public String savePost(
+            @RequestParam(name = "title") String title,
+            @RequestParam(name = "body") String body,
+            Model model
+    ) {
+        User user = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        Post post = new Post(title, body, user);
         postSvc.save(post);
-        return "redirect:/posts/";
+        model.addAttribute("post", post);
+        return "posts/create";
+    }
+
+    @Value("${file-upload-path}")
+    private String uploadPath;
+
+    @GetMapping("/fileupload")
+    public String showUploadFileForm() {
+        return "fileupload";
+    }
+
+    @PostMapping("/fileupload")
+    public String saveFile(
+            @RequestParam(name = "file") MultipartFile uploadedFile,
+            Model model
+    ) {
+        String filename = uploadedFile.getOriginalFilename();
+        String filepath = Paths.get(uploadPath, filename).toString();
+        File destinationFile = new File(filepath);
+        try {
+            uploadedFile.transferTo(destinationFile);
+            model.addAttribute("message", "File successfully uploaded!");
+        } catch (IOException e) {
+            e.printStackTrace();
+            model.addAttribute("message", "Oops! Something went wrong! " + e);
+        }
+
+        return "fileupload";
     }
 
     @GetMapping("/posts/{id}/edit")
@@ -60,22 +107,29 @@ public class PostsController {
         return "posts/edit";
 
     }
+
     @PostMapping("/posts/{id}/edit")
-    public String editPost(@ModelAttribute Post post){
+    public String editPost(@ModelAttribute Post post) {
         postSvc.save(post);
-        return "redirect:/posts";
+        return "redirect:/posts/" + post.getId();
     }
 
-    @GetMapping("/posts/create")
-    public String savePost(Model model) {
-        model.addAttribute("post", new Post());
-        return "posts/create";
-    }
 
-    @PostMapping("post/delete")
-    public String deletePost(@ModelAttribute Post post) {
+    @PostMapping("/post/delete")
+    public String deletePost(@ModelAttribute Post post, Model model) {
         postSvc.deletePost(post.getId());
-        return "redirect:/posts";
+        model.addAttribute("msg", "Your post was deleted correctly");
+        return "return the view with a success message";
     }
 
+    @GetMapping("/posts.json")
+    public @ResponseBody Iterable<Post> viewAllAds() {
+        return postSvc.findAll();
+    }
+
+    @GetMapping("/posts/ajax")
+    public String viewAllAdsUsingAnAjaxCall() {
+        return "posts/ajax";
+    }
 }
+
