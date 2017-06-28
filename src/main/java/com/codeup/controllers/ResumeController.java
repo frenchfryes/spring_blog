52bc09133947cfb65d405e98df36a84167ff6a7f
frenchfryes <derekwfrye@gmail.com>
2017-06-28 14:54:08 -0500

    changed home controller to Resume Controller

--- src/main/java/com/codeup/controllers/ResumeController.java
+++ src/main/java/com/codeup/controllers/ResumeController.java
@@ -7,10 +7,10 @@ import org.springframework.web.bind.annotation.GetMapping;
  * Created by frenchfryes on 6/19/17.
  */
 @Controller
-public class HomeController {
-    @GetMapping("/resume")
+public class ResumeController {
+    @GetMapping("/pdf/web/viewer.html?file=ResumeoftheYear.pdf")
     public String resume(){
-        return "resume";
+        return "/resume";
     }
 
 }
